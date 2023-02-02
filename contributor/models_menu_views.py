a6f7f6a952d722f2569523d958e8f1c56721ead0
Bijay Pokhrel <pokhrelbijay44@gmail.com>
2023-02-02 13:26:05 +0545

    react eg 2 add and save data

--- contributor/models_menu_views.py
+++ contributor/models_menu_views.py
@@ -3,6 +3,7 @@ from .models import Claim_Booking
 import json
 from django.http import JsonResponse
 import datetime
+from .models_menu import Menu
 
 
 # Create your views here.
@@ -13,7 +14,20 @@ def menu(request):
 
 
 def app_menus(request):
-    return JsonResponse([{}, {}], safe=False)
+    if request.method == "POST":
+        data = json.loads(request.body)
+        menu_text = data.get('menu_text')
+        menu_link = data.get('menu_link')
+        menu = Menu(menu_text=menu_text, menu_link=menu_link)
+        menu.save()
+    # return JsonResponse([{}, {}], safe=False)
+    menus = []
+    for menu in Menu.objects.all():
+        menus.append({
+            'menu_text': menu.menu_text,
+            'menu_link': menu.menu_link,
+        })
+    return JsonResponse(menus, safe=False)
 
 
 def menu_add(request):
